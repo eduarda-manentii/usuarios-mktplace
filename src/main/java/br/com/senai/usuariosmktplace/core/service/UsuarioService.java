d1b479552ebf323adf34320cc9cf0c2d330df779
Eduarda Manenti <dudamanenti65@gmail.com>
2023-08-24 20:51:02 -0300

    Modificado uma validação para usar o google guava.

--- src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
@@ -40,8 +40,8 @@ public class UsuarioService {
 
 	private List<String> fracionar(String nomeCompleto) {
 		List<String> nomeFracionado = new ArrayList<String>();
-		nomeCompleto = nomeCompleto.trim();
-		if (nomeCompleto != null && !nomeCompleto.isBlank()) {
+		if (Strings.isNullOrEmpty(nomeCompleto)) {
+			nomeCompleto = nomeCompleto.trim();
 			String[] partesDoNome = nomeCompleto.split(" ");
 			for (String parte : partesDoNome) {
 				boolean isNaoContemArtigo = !(parte.equalsIgnoreCase("de") || parte.equalsIgnoreCase("e")
