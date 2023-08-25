dc17c72fe3f6e8813b6cce5f4de7be67cef570e9
Eduarda Manenti <dudamanenti65@gmail.com>
2023-08-25 19:53:32 -0300

    Refatoração do domínio para uso do Lombok.

--- src/main/java/br/com/senai/usuariosmktplace/core/domain/Usuario.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/domain/Usuario.java
@@ -1,47 +1,24 @@
 package br.com.senai.usuariosmktplace.core.domain;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import lombok.Getter;
-import lombok.Setter;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+@Data
 public class Usuario {
 
-	@Getter @Setter
+	@EqualsAndHashCode.Include
 	private String login;
 	
-	@Getter @Setter
 	private String senha;
 	
-	@Getter @Setter
+	@ToString.Include
 	private String nomeCompleto;
 	
-	public Usuario(String login, String senha, String nomeCompleto) {
-		this.login = login;
-		this.senha = senha;
-		this.nomeCompleto = nomeCompleto;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(login);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(login, other.login);
-	}
-	
-	@Override
-	public String toString() {
-		return "Nome do Usuario: " + getNomeCompleto() + "\nLogin do usuário: " + getLogin();
-	}
-
 }
