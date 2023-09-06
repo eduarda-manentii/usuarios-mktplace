bf1b9ffe041c6cebda0ada800c5fbbf05f999d27
Eduarda Manenti <eduarda_manenti@nuntec.com.br>
2023-09-06 09:15:53 -0300

    Criado o método atualizar no repositório do usuário.

--- src/main/java/br/com/senai/usuariosmktplace/repository/UsuarioRepository.java
+++ src/main/java/br/com/senai/usuariosmktplace/repository/UsuarioRepository.java
@@ -1,11 +1,17 @@
 package br.com.senai.usuariosmktplace.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.senai.usuariosmktplace.entity.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+	
+	@Modifying
+	@Query(value = "UPDATE Usuario u SET u.nome = :nome, u.senha = :senha WHERE u.login = :login")
+	public void atualizarPor(String login, String nome, String senha);
 
 }
